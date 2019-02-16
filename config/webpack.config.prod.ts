b89a17d51371be9dd623d726226e0e71def6f1f6
hdorgeval <henri.dorgeval@outlook.fr>
2019-02-16 15:02:37 +0100

    feat(app): be able to analyze app bundle with webpack-bundle-analyzer

--- config/webpack.config.prod.ts
+++ config/webpack.config.prod.ts
@@ -1,9 +1,17 @@
 import {Configuration} from 'webpack';
 import merge from 'webpack-merge';
 import webpackCommonConfig from './webpack.config.common';
+import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer';
 
 const config: Configuration = merge(webpackCommonConfig,{
     mode: 'production',
+    plugins: [
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'static',
+            openAnalyzer: false,
+            reportFilename: 'bundle-analyzer-report.html'
+        }),
+    ]
 });
 
 export default config;
